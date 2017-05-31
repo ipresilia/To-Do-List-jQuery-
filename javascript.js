6079251af44fdaf985bedc54616db6e024e841d6
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-31 15:16:10 +0200

    Updated all of the todo counters

--- javascript.js
+++ javascript.js
@@ -8,4 +8,15 @@ $(document).ready(function() {
   $("input[type=checkbox]").bind('change', toggleDone);
 });
 
-$("#total-count").html($(".todo").length);
+// $("#total-count").html($(".todo").length);
+
+function updateCounters() {
+  var todoCount = $(".todo").length;
+  var completedCount = $(".completed").length
+
+    $("#total-count").html(todoCount);
+    $("#completed-count").html(completedCount);
+    $("#todo-count").html(todoCount - competedCount);
+}
+
+updateCounters();
