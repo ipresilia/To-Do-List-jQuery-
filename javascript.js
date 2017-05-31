5b8f183de4df39cc9e7372227c19f28a19e9442d
ipresilia <ilsmarie.presilia@hotmail.com>
2017-05-31 16:01:13 +0200

    Created todos

--- javascript.js
+++ javascript.js
@@ -16,7 +16,55 @@ function updateCounters() {
 
     $("#total-count").html(todoCount);
     $("#completed-count").html(completedCount);
-    $("#todo-count").html(todoCount - competedCount);
+    $("#todo-count").html(todoCount - completedCount);
 }
 
 updateCounters();
+
+function submitTodo(event) {
+  // This stops the form from doing the default action, submitting
+  event.preventDefault();
+
+  var title = $("#new-todo").val();
+
+  createTodo(title);
+
+  $("#new-todo").val(null);
+  updateCounters();
+
+  function createTodo(title) {
+  var checkboxId = "todo-" + nextTodoId();
+
+  var listItem = $("<li></li>");
+  listItem.addClass("todo");
+
+  var checkbox = $('<input>');
+  checkbox.attr('type', 'checkbox');
+  checkbox.attr('id', checkboxId);
+  checkbox.val(1);
+  checkbox.bind('change', toggleDone);
+
+  var space = document.createTextNode(" ");
+
+  var label = $('<label></label>');
+  label.attr('for', checkboxId);
+  label.html(title);
+
+  listItem.append(checkbox);
+  listItem.append(space);
+  listItem.append(label);
+
+  $("#todolist").append( listItem );
+
+  updateCounters();
+}
+
+function nextTodoId() {
+  return $(".todo").length + 1;
+}
+
+}
+
+
+
+$("form").bind('submit', submitTodo);
